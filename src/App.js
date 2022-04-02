d884e47454c56423c428fc3f06c7a5eec4402cb8
JayB1308 <jbudhadev@gmail.com>
2022-04-02 20:28:46 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import Free from "./components/Free";
+import Client from "./components/Client";
+import SuperRare from "./components/SuperRare";
+import Release from "./components/Release";
+import Signup from "./components/Signup";
+import Like from "./components/Like";
+import Footer from "./components/Footer";
+import "./scss/index.scss";
+import Home from "./components/Home";
+import scrollreveal from "scrollreveal";
 
 function App() {
+
+  const [theme,setTheme] = useState("dark");
+  const changeTheme = () => {
+    theme === "dark"? setTheme("light"): setTheme("");
+  }
+
+
+  useEffect(() => {
+    const registerAnimation = () => {
+      const sr = scrollreveal({
+        origin:"bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false
+      });
+      sr.reveal(`
+      nav,.home,.free,.clients,.super-rare,.releases, .like, .signup,footer
+      `,
+      {
+        interval:500
+      })
+    }
+    registerAnimation();
+  }, []);
+
+  window.setTimeout(() => {
+    const home = document.getElementsByClassName("home");
+    home[0].style.transform = "none";
+    const nav = document.getElementsByTagName("nav");
+    nav[0].style.transform = "none";
+  },1500)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container"
+    data-theme ={theme}>
+    <ScrollToTop />
+    <Navbar changeTheme ={changeTheme} currentTheme = {theme}/>
+    <Home />
+    <Free />
+    <Client />
+    <SuperRare />
+    <Release />
+    <Like />
+    <Signup />
+    <Footer />
     </div>
   );
 }
